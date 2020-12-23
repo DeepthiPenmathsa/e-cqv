d1992602564d93bcfdf43a0131193ce8b49ee2c5
Deepthi <vndeepthi.p@gmail.com>
2020-12-23 18:57:58 +0530

    FirstCommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Headder from './Headder';
+import Sider from './Sider';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Starting E-CQA Application </h1>
+      <CssBaseline>
+      {/* <h1>Starting E-CQA Application </h1> */}
+      <Headder/>
+      <Sider/>
+      </CssBaseline>
     </div>
   );
 }
