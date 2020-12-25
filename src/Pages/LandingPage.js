21c20360effdd67b503730d8caa3a38ee2db8c34
Deepthi <vndeepthi.p@gmail.com>
2020-12-25 17:16:09 +0530

    Changes to access DB Data

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,13 @@
+import Headder from '../components/Headder';
+import Sider from '../components/Sider';
+
+export default function Landing()
+{
+    return(
+        <div>
+            <Headder/>
+      <Sider/> 
+        </div>
+       
+    )
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Headder from './Headder';
-import Sider from './Sider';
 
+import Login from '../Pages/Login'
+import Landing from '../Pages/LandingPage'
+import { ApolloProvider} from '@apollo/client';
+import {createApolloClient} from '../DBConnections/ApolloClient';
+import { BrowserRouter } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 
 
 function App() {
+  const client = createApolloClient();
   return (
     <div className="App">
+    <BrowserRouter>
+    <ApolloProvider client={client}>
+   
       <CssBaseline>
-      {/* <h1>Starting E-CQA Application </h1> */}
-      <Headder/>
-      <Sider/>
+    <Switch>
+            <Route exact path="/">
+                 <Login/>
+                
+            </Route>
+            <Route path="/landing">
+               <Landing/>
+            </Route>
+          </Switch>
+    
+      
+      
       </CssBaseline>
+    
+    </ApolloProvider>
+    </BrowserRouter>
     </div>
   );
 }
